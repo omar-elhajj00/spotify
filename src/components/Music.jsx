b4f53274d2a2e6ba97a2adc6e07af763833a99a3
omar-elhajj00 <omar.elhajj.00@gmail.com>
2022-10-15 12:37:09 +0300

    add the searchArtist function

--- src/components/Music.jsx
+++ src/components/Music.jsx
@@ -1,8 +1,38 @@
 import React , {useEffect , useState} from "react";
 import '../music.css';
 import searchIcon from './search.svg';
+import axios from 'axios';
+
 
 const Music = () => {
+
+    // fetching data
+    // seacrh state 
+    const [searchKey, setSearchKey] = useState("");
+
+    //artist state
+    const [artists, setArtists] =useState([]);
+    const token=window.localStorage.getItem("token");
+    console.log("tokens are" ,token);
+
+    //api
+    const searchArtists = async (e) => {
+        e.preventDefault();
+        const {data} = await axios.get("https://api.spotify.com/v1/search",{
+        headers: {
+            Authorization: `Bearer ${token}`
+        },
+        params: {
+            q: searchKey,
+            type:"artist"
+        }}
+        
+        )
+        console.log(data);
+    
+}
+   
+
     return(
         <div className="app">
             <h1>Spotify </h1>
